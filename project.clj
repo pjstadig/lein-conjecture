6f86cfc99b9388f0c27d643c4668ca120cb003a8
Paul Stadig <paul@stadig.name>
2013-03-06 16:50:57 -0500

    Adds "test" alias.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject name.stadig/lein-conjecture "0.1.0"
   :description "leiningen plugin for conjecture"
   :url "http://github.com/pjstadig/lein-conjecture"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[name.stadig/conjecture "0.1.0"]])
+  :dependencies [[name.stadig/conjecture "0.1.0"]]
+  :aliases {"test" "conjecture"})
