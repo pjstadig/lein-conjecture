ab72e909538300bd9df195dab16b65c89587e41e
Paul Stadig <paul@stadig.name>
2013-03-06 16:43:06 -0500

    More name changing.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject name.stadig/lein-conjecture "0.1.0"
-  :description "leiningen plugin for clojure-test"
-  :url "http://github.com/pjstadig/lein-clojure-test"
+  :description "leiningen plugin for conjecture"
+  :url "http://github.com/pjstadig/lein-conjecture"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
