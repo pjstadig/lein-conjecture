5c45becd1f1db132459f7700f289210e7cb85824
Paul Stadig <paul@stadig.name>
2013-03-07 06:20:51 -0500

    conjecture should be a dev dependency.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[name.stadig/conjecture "0.1.0"]]
+  :profiles {:dev {:dependencies [[name.stadig/conjecture "0.1.0"]]}}
   :aliases {"test" "conjecture"})
