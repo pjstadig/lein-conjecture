79b3c9a646a888568def991316fa0158faa7909c
Paul Stadig <paul@stadig.name>
2013-03-11 19:45:58 -0400

    Actually, conjecture should be a plain dependency.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :profiles {:dev {:dependencies [[conjecture "0.3.0-SNAPSHOT"]]}}
+  :dependencies [[conjecture "0.3.0-SNAPSHOT"]]
   :aliases {"test" "conjecture"})
